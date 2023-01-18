ea884560eff25f3fc4634c86bcd3c59563616344
rafaelftourinho <rafaelftourinho@gmail.com>
2023-01-18 20:22:47 -0300

    :sparkles: req 9

---
+++ src/Monster.ts
@@ -0,0 +1,32 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    this._lifePoints = attackPoints - this.lifePoints;
+
+    if (this.lifePoints <= 0) this._lifePoints = -1;
+
+    return this.lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    const damageGiven = this.strength;
+    enemy.receiveDamage(damageGiven);
+  }
+}
