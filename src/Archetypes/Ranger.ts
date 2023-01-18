b77a77a826184d4bb5c36480f750409773f3f811
rafaelftourinho <rafaelftourinho@gmail.com>
2023-01-18 17:51:08 -0300

    :sparkles: req 5

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  private static _instanceCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger._instanceCount += 1;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return this._instanceCount;
+  }
+}
