9f5b19e0d1c448630310b7932ad56c9b7315ce0f
rafaelftourinho <rafaelftourinho@gmail.com>
2023-01-19 18:44:56 -0300

    :sparkles: req 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,20 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _p2: Fighter;
+
+  constructor(p1: Fighter, p2: Fighter) {
+    super(p1);
+    this._p2 = p2;
+  }
+
+  fight(): number {
+    while (
+      this.player.lifePoints > 0 && this._p2.lifePoints > 0) {
+      this.player.attack(this._p2);
+      this._p2.attack(this.player);
+    }
+    return super.fight();
+  }
+}
